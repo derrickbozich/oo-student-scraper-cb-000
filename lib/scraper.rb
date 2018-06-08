63a49e9f9006e4a731ee1f394cb0235623b7657d
derrick b <dbozich1@gmail.com>
2018-06-08 08:08:35 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,49 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    student_array = []
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css(".student-card a").each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = "#{student.attr('href')}"
+      student_array.push({:name => name, :location => location, :profile_url => profile_url})
+    end
+    student_array
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    profile_quote = doc.css(".profile-quote").text
+    bio = doc.css(".description-holder p").text
+    links = doc.css(".social-icon-container a")
+    link_array = []
+    links.each do |link|
+      link_array.push("#{link.attr('href')}")
+    end
+
+    attributes = {}
+
+    link_array.each do |link|
+      if link.include?("twitter")
+        attributes[:twitter]=link
+      elsif link.include?("linkedin")
+        attributes[:linkedin]=link
+      elsif link.include?("github")
+        attributes[:github]=link
+      elsif link
+        attributes[:blog]=link
+      end
+    end
+
+    attributes[:profile_quote]=profile_quote
+    attributes[:bio]=bio
+
+    attributes
   end
-
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,30 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    # Scraper.scrape_index_page(index_url)
+    students_array.each do |student_info|
+      student = Student.new(student_info)
+      student_info.each {|key, value| student.send(("#{key}="), value)}
+      @@all << student
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|k,v| self.send(("#{k}="), v)}
+
+
   end
 
   def self.all
-    
+    @@all
   end
 end
-
