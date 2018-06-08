5826c3cf47a6661baeadeae92340f0201a663b96
derrick b <dbozich1@gmail.com>
2018-06-08 08:14:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,18 +10,14 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    # Scraper.scrape_index_page(index_url)
     students_array.each do |student_info|
       student = Student.new(student_info)
       student_info.each {|key, value| student.send(("#{key}="), value)}
-      @@all << student
     end
   end
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each {|k,v| self.send(("#{k}="), v)}
-
-
   end
 
   def self.all
